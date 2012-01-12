50721b896b2cc937f0e5719fc146e001195ccf3b
sepiariver <info@sepiariver.com>
2012-01-11 21:58:32 -0800

    Just mucking around.

--- _build/widget.data.php
+++ _build/widget.data.php
@@ -4,9 +4,10 @@ $widgets[1]= $modx->newObject('modDashboardWidget');
 $widgets[1]->fromArray(array (
   'name' => 'flowdashboard.title',
   'description' => 'flowdashboard.description',
-  'type' => 'file',
+  'type' => 'html',
   'size' => 'double',
-  'content' => '[[++core_path]]components/flowdashboard/widget.php',
+  'content' => '<!--Flow Dashboard Widget by Sepia River-->
+<iframe src="http://link.getflow.com/SFM" title="Flow Task Management" style="width:100%;height:100%;min-height:600px;"></iframe>',
   'namespace' => 'flowdashboard',
   'lexicon' => 'flowdashboard:default',
 ), '', true, true);
